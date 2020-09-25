bf6e4c56dec2160af10439be9c7baa6bfe4ccd48
Tommm3 <ssarkof@gmail.com.com>
2020-09-25 11:50:15 +0200

    dzis

--- Python_android/silka/main.py
+++ Python_android/silka/main.py
@@ -5,9 +5,11 @@ from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from kivy.uix.textinput import TextInput
 from datetime import datetime
 import pandas as pd
 
@@ -35,11 +37,19 @@ def read_day_weight(wd):
     print(len(reader.columns)-1)
     return df.to_string(index=False).replace('\n','\n\n')
 
-class MySilka(Widget):
+class MySilka(BoxLayout):
     weekday = ObjectProperty(None)
     exercises = ObjectProperty(None)
     last_weight = ObjectProperty(None)
     # new_weight[] = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(MySilka,self).__init__(**kwargs)
+        self.cols = 2
+        self.size_hint_x = 1.7
+        self.padding = 50
+        self.new_weights = TextInput(multiline=False)
+        self.add_widget(self.new_weights)
     def get_weekday(self):
         self.weekday.text = datetime.now().strftime("%A")
     def get_exercises(self):
@@ -51,9 +61,10 @@ class MySilka(Widget):
 class MyApp(App):
     def build(self):
         apka = MySilka()
+        # self.height = TextInput(text='enter', size_hint=(200,200))
+        # apka.add_widget(self.height)
         apka.get_weekday()
         apka.get_exercises()
         return apka
-
 if __name__ == "__main__":
     MyApp().run()

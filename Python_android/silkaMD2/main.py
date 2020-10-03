89ed169d89ef0ebe577cf5b6dc63d576e4e1d936
Tommm3 <ssarkof@gmail.com.com>
2020-10-03 08:40:26 +0200

    3.10 dom

--- Python_android/silkaMD2/main.py
+++ Python_android/silkaMD2/main.py
@@ -17,107 +17,28 @@ from helpers import username_helper, button_helper, list_helper, text_field_help
 from datetime import datetime
 import pandas as pd
 from openpyxl import load_workbook
-#
-# KV = '''
-# <Content>
-#     text: "Enter"
-#
-# '''
 
 
 def get_init_exercise_tuple():
     wd = datetime.now().strftime("%A")
     reader = pd.read_excel(r'silka.xlsx')
     if wd=='Monday':
-        df = pd.read_excel('silka.xlsx', skiprows=0, usecols=[0,1,len(reader.columns)-1], nrows=8)
+        df = pd.read_excel('silka.xlsx', skiprows=0, usecols=[0,1,len(reader.columns)-1,len(reader.columns)], nrows=8)
+        df.index+=1
     elif wd=='Tuesday':
-        df = pd.read_excel('silka.xlsx', skiprows=8, usecols=[0,1,len(reader.columns)-2], nrows=9)
+        df = pd.read_excel('silka.xlsx', skiprows=8, usecols=[0,1,len(reader.columns)-2,len(reader.columns)-1], nrows=9)
+        df.index+=1
     elif wd=='Thursday':
-        df = pd.read_excel('silka.xlsx', skiprows=17, usecols=[0,1,len(reader.columns)-2], nrows=8)
+        df = pd.read_excel('silka.xlsx', skiprows=17, usecols=[0,1,len(reader.columns)-2,len(reader.columns)-1], nrows=8)
+        df.index+=1
     elif wd=='Friday':
         df = pd.read_excel('silka.xlsx', skiprows=25, usecols=[0,1,len(reader.columns)-2,len(reader.columns)-1], nrows=5)
+        df.index+=1
     else:
         df = 'Brak świczeń na dziś'
-    print(df.to_records(index=False))
-    return df.to_records(index=False)
+    print(df.to_records(index=True))
+    return df.to_records(index=True)
 
-# class MySilka(BoxLayout):
-#     weekday = ObjectProperty(None)
-#     exercises = ObjectProperty(None)
-#     last_weight = ObjectProperty(None)
-#
-#     def __init__(self, **kwargs):
-#         # weekday_in = datetime.now().strftime('%A')
-#         weekday_in = datetime.now().strftime('%A')
-#         super(MySilka,self).__init__(**kwargs)
-#         self.size_hint_x = 1.7
-#         self.padding = 100
-#         self.new_weights = GridLayout(cols=1, spacing=20)
-#         global new_weight1
-#         new_weight1=TextInput(multiline=False)
-#         global new_weight2
-#         new_weight2=TextInput(multiline=False)
-#         global new_weight3
-#         new_weight3=TextInput(multiline=False)
-#         global new_weight4
-#         new_weight4=TextInput(multiline=False)
-#         global new_weight5
-#         new_weight5=TextInput(multiline=False)
-#         self.new_weights.add_widget(new_weight1)
-#         self.new_weights.add_widget(new_weight2)
-#         self.new_weights.add_widget(new_weight3)
-#         self.new_weights.add_widget(new_weight4)
-#         self.new_weights.add_widget(new_weight5)
-#         if weekday_in == "Monday" or weekday_in == "Thursday" or weekday_in == "Tuesday":
-#             global new_weight6
-#             new_weight6=TextInput(multiline=False)
-#             global new_weight7
-#             new_weight7=TextInput(multiline=False)
-#             global new_weight8
-#             new_weight8=TextInput(multiline=False)
-#             self.new_weights.add_widget(new_weight6)
-#             self.new_weights.add_widget(new_weight7)
-#             self.new_weights.add_widget(new_weight8)
-#             print(weekday_in)
-#             if weekday_in == "Tuesday":
-#                 global new_weight9
-#                 new_weight9=TextInput(multiline=False)
-#                 self.new_weights.add_widget(new_weight9)
-#         self.add_widget(self.new_weights)
-#
-#     def get_weekday(self):
-#         self.weekday.text = datetime.now().strftime('%A')
-#     def get_exercises(self):
-#         self.exercises.text = read_day(self.weekday.text)
-#         self.last_weight = read_day_weight(self.weekday.text)
-#         print(self.exercises.text, 'utf-8')
-#         print(self.last_weight, 'utf-8')
-#     def btn(self):
-#         masa = [new_weight1.text,new_weight2.text,new_weight3.text,new_weight4.text,new_weight5.text]
-#         if self.weekday.text == 'Monday' or self.weekday.text == 'Thursday':
-#             masa.extend([new_weight6.text,new_weight7.text,new_weight8.text])
-#         elif self.weekday.text == 'Tuesday':
-#             masa.extend([new_weight6.text,new_weight7.text,new_weight8.text,new_weight9.text])
-#         elif self.weekday.text == 'Friday':
-#             pass
-#         else:
-#             exit()
-#         writer = pd.ExcelWriter('silka.xlsx', engine='openpyxl')
-#         writer.book = load_workbook('silka.xlsx')
-#         writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
-#         reader = pd.read_excel(r'silka.xlsx')
-#         masa_dict = {('masa'+str(len(reader.columns)-2)):masa}
-#         df = pd.DataFrame(masa_dict)
-#         if self.weekday.text == 'Monday':
-#             df.to_excel(writer,index=False,startcol=len(reader.columns))
-#         elif self.weekday.text == 'Tuesday':
-#             df.to_excel(writer,index=False,startcol=len(reader.columns)-1,startrow=9,header=False)
-#         elif self.weekday.text == 'Thursday':
-#             df.to_excel(writer,index=False,startcol=len(reader.columns)-1,startrow=18,header=False)
-#         elif self.weekday.text == 'Friday':
-#             df.to_excel(writer,index=False,startcol=len(reader.columns)-1,startrow=26,header=False)
-#         writer.close()
-#         print(masa)
 
 class Content(Widget):
     pass
@@ -125,23 +46,24 @@ class Content(Widget):
 
 class MyApp(MDApp):
     def build(self):
+        self.current_table = get_init_exercise_tuple()
         self.theme_cls.primary_palette = "Yellow"
         self.theme_cls.primary_hue = "A700"
         self.theme_cls.theme_style = "Dark"
-        screen = Screen()
-        table = MDDataTable(pos_hint={"center_x":0.5,"center_y":0.5},
+        self.screen = Screen()
+        self.table = MDDataTable(pos_hint={"center_x":0.5,"center_y":0.5},
                             size_hint=(0.9,0.6),
-                            check="True",
                             rows_num=9,
                             column_data=[
+                                ("No.",dp(8)),
                                 ("Exercise",dp(80)),
                                 ("Repeats",dp(20)),
                                 ("Last weight",dp(20)),
                                 ("New weight",dp(20))
                             ],
-                            row_data=get_init_exercise_tuple()
+                            row_data=self.current_table,
                             )
-        table.bind(on_row_press=self.row_press)
+        self.table.bind(on_row_press=self.row_press)
         head = MDLabel(text = datetime.now().strftime('%A'),
                         halign = 'center',
                         font_style = 'H3',
@@ -149,27 +71,52 @@ class MyApp(MDApp):
                         theme_text_color="Custom",
                         text_color=self.theme_cls.primary_color
                         )
-        screen.add_widget(head)
-        screen.add_widget(table)
-        return screen
+        self.confirm = MDRectangleFlatButton(text="CONFIRM",
+                                    on_release=self.confirm_action,
+                                    text_color=self.theme_cls.primary_color,
+                                    pos_hint={"center_x":0.8,"center_y":0.1}
+                                    )
+        self.screen.add_widget(head)
+        self.screen.add_widget(self.table)
+        self.screen.add_widget(self.confirm)
+        return self.screen
 
-    def row_press(self, instance_table, current_row):
+    def row_press(self, instance_table, instance_row):
+        self.row_no = int(instance_row.index/5)
         self.weight = Builder.load_string(text_field_helper)
-        self.dialog = MDDialog(text=current_row.text + "\n\n",
+        self.dialog = MDDialog(text=instance_row.text + "\n\n",
                         size_hint=(0.8,1),
                         buttons=[MDRectangleFlatButton(text="OK",
                                                     on_release=self.close_dialog,
                                                     text_color=self.theme_cls.primary_color,
                                                     pos_hint={"center_x":1,"center_y":0.5})]
                                 )
-
         self.dialog.add_widget(self.weight)
         self.dialog.open()
 
     def close_dialog(self, obj):
-        print(self.weight.text)
+        if self.weight.text != "":
+            list_current_table_row = list(self.current_table[self.row_no])
+            list_current_table_row[4] = self.weight.text
+            self.current_table[self.row_no] = tuple(list_current_table_row)
+            self.table = MDDataTable(pos_hint={"center_x":0.5,"center_y":0.5},
+                                size_hint=(0.9,0.6),
+                                rows_num=9,
+                                column_data=[
+                                    ("No.",dp(8)),
+                                    ("Exercise",dp(80)),
+                                    ("Repeats",dp(20)),
+                                    ("Last weight",dp(20)),
+                                    ("New weight",dp(20))
+                                ],
+                                row_data=self.current_table
+                                )
+            self.table.bind(on_row_press=self.row_press)
+            self.screen.add_widget(self.table)
         self.dialog.dismiss()
 
+    def confirm_action(self):
+        pass
 
 if __name__ == "__main__":
     MyApp().run()
--- second.py
+++ second.py
@@ -1,4 +1,3 @@
+from datetime import datetime
 
-
-masa = {('masa'+str(1)): [1,2,3,4,5,6,75]}
-print(masa)
+print(datetime.now().weekday())
